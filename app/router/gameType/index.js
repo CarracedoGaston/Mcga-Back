d626ad24374380b9a484131e1002b93e9c6e0721
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-05 22:25:22 -0300

    basic structure of question and gameType

---
+++ app/router/gameType/index.js
@@ -0,0 +1,24 @@
+// const express = require('express')
+// const controller = require('./controller')
+
+// const router = express.Router()
+// const  {
+//   getAll,
+//   getById,
+//   insert,
+//   upsert,
+//   update,
+//   remove,
+//   signIn
+// } = controller
+
+// router.use(express.json())
+
+// router.get('/', getAll)
+// router.get('/:id', getById)
+// router.post('/', insert)
+// router.put('/:id', upsert)
+// router.patch('/:id', update)
+// router.delete('/:id', remove)
+
+// module.exports = router
--- app/router/index.js
+++ app/router/index.js
@@ -6,5 +6,7 @@ const gameType = require('./gameType')
 const router = express.Router()
 
 router.use('/user', user)
+// router.use('/question', question)
+// router.use('/gametype', gameType)
 
 module.exports = router
