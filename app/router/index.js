8ee1feda11bb8011f0be5ec98eb19b7601cc4ff3
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-05 21:06:02 -0300

    basic router structure

---
+++ app/router/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const user = require('./user')
+const question = require('./question')
+const gameType = require('./ogameTyperder')
+
+const router = express.Router()
+
+router.use('/user', user)
+router.use('/question', question)
+router.use('/gameType', gameType)
+
+module.exports = router
