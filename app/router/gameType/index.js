8575f96a45629ebc34c26877d347ff9a3c0bb430
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-11-05 21:23:13 -0300

    gameType && question router methods

---
+++ app/router/gameType/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const controller = require('./controller')
+
+const router = express.Router()
+const  {
+  getAll,
+  getById,
+  insert,
+  remove,
+} = controller
+
+router.use(express.json())
+
+router.get('/', getAll)
+router.get('/:id', getById)
+router.post('/', insert)
+router.delete('/:id', remove)
+
+module.exports = router
+
