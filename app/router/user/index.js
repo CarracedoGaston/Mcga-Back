aeda5ae52e8a9d9fdfdb184d40f8df11ef07e9e2
yaninamontella <yani_montella@hotmail.com>
2019-12-02 18:19:17 -0300

    add secret_token to config

--- app/router/user/index.js
+++ app/router/user/index.js
@@ -9,7 +9,8 @@ const  {
   upsert,
   update,
   remove,
-  signIn
+  signIn,
+  signUp
 } = controller
 
 router.use(express.json())
@@ -18,6 +19,7 @@ router.get('/', getAll)
 router.get('/:id', getById)
 router.post('/', insert)
 router.post('/signIn', signIn)
+router.post('/signUp', signUp)
 router.put('/:id', upsert)
 router.patch('/:id', update)
 router.delete('/:id', remove)
