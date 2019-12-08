9a9d36ab8cc809aa3fc2c974d8c03184e2eea1d8
yaninamontella <yani_montella@hotmail.com>
2019-12-08 15:25:13 -0300

    install cors for DELETE METHOD

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const router = require('./app/router')
+const cors = require('cors')
 
 const app = express ()
 const port = 5000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+app.use(cors())
+
+
 
 const mongoDBURL = require('./config/database.config')
 
